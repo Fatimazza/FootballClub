26dc5f9b3eca0845e064764645f7305ee5f1517b
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-23 17:28:37 +0700

    Main Adapter - Make Adapter implement ViewHolder

--- app/src/main/java/io/github/fatimazza/footballclub/main/MainAdapter.kt
+++ app/src/main/java/io/github/fatimazza/footballclub/main/MainAdapter.kt
@@ -2,9 +2,23 @@ package io.github.fatimazza.footballclub.main
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.view.ViewGroup
 import io.github.fatimazza.footballclub.model.Team
 
-class MainAdapter(private val teams: List<Team>) {
+class MainAdapter(private val teams: List<Team>)
+    : RecyclerView.Adapter<TeamViewHolder>() {
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): TeamViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(p0: TeamViewHolder, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
 }
 
