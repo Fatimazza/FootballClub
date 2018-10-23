b0717090f919d019f188a04bd258d8f116c3a3a5
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-23 17:34:05 +0700

    Main Adapter - Add Layout for Item of RecyclerView

--- app/src/main/java/io/github/fatimazza/footballclub/main/MainAdapter.kt
+++ app/src/main/java/io/github/fatimazza/footballclub/main/MainAdapter.kt
@@ -4,6 +4,9 @@ import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
 import io.github.fatimazza.footballclub.model.Team
+import org.jetbrains.anko.AnkoComponent
+import org.jetbrains.anko.AnkoContext
+import org.jetbrains.anko.linearLayout
 
 class MainAdapter(private val teams: List<Team>)
     : RecyclerView.Adapter<TeamViewHolder>() {
@@ -20,6 +23,14 @@ class MainAdapter(private val teams: List<Team>)
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+}
+
+class TeamUI: AnkoComponent<ViewGroup> {
+    override fun createView(ui: AnkoContext<ViewGroup>): View {
+        return with(ui){
+            linearLayout {  }
+        }
+    }
 }
 
 class TeamViewHolder(view: View): RecyclerView.ViewHolder(view) {
