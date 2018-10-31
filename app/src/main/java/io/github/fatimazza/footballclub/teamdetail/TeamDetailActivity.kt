48bcf1bb56e061df127bc585f2ed5dd7dfaa91be
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-31 21:48:23 +0700

    Team Detail - Add Toolbar title and Up Navigation

--- app/src/main/java/io/github/fatimazza/footballclub/teamdetail/TeamDetailActivity.kt
+++ app/src/main/java/io/github/fatimazza/footballclub/teamdetail/TeamDetailActivity.kt
@@ -3,14 +3,17 @@ package io.github.fatimazza.footballclub.teamdetail
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.NavUtils
 import android.support.v4.content.ContextCompat
 import android.support.v4.widget.SwipeRefreshLayout
 import android.view.Gravity
+import android.view.MenuItem
 import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.ProgressBar
 import android.widget.TextView
+import io.github.fatimazza.footballclub.R
 import io.github.fatimazza.footballclub.R.color.colorAccent
 import io.github.fatimazza.footballclub.R.color.colorPrimaryDark
 import org.jetbrains.anko.*
@@ -30,6 +33,9 @@ class TeamDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         TeamDetailUI().setContentView(this)
+
+        supportActionBar?.title = getString(R.string.team_detail)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     class TeamDetailUI: AnkoComponent<TeamDetailActivity> {
@@ -94,5 +100,15 @@ class TeamDetailActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                NavUtils.navigateUpFromSameTask(this)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
