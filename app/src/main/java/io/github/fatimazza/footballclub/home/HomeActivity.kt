5235063791bf91afeb8cc23b1f2005fecadec13f
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-31 19:40:28 +0700

    Home Activity - Set Navigation for Bottom Navigation, Change first launch to Home

--- app/src/main/java/io/github/fatimazza/footballclub/home/HomeActivity.kt
+++ app/src/main/java/io/github/fatimazza/footballclub/home/HomeActivity.kt
@@ -3,11 +3,23 @@ package io.github.fatimazza.footballclub.home
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import io.github.fatimazza.footballclub.R
+import io.github.fatimazza.footballclub.R.id.favorites
+import io.github.fatimazza.footballclub.R.id.teams
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        bottom_navigation.setOnNavigationItemReselectedListener {
+            item -> when(item.itemId) {
+                teams -> {}
+                favorites -> {}
+            }
+            true
+        }
+        bottom_navigation.selectedItemId = teams
     }
 }
