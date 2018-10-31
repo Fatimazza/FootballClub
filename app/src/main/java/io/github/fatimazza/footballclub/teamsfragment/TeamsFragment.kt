e890dcb5db7053a01532f4f4f1dd96916a3ffa3c
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-31 21:02:48 +0700

    Teams Fragment - Add functions same as Main Activity

--- app/src/main/java/io/github/fatimazza/footballclub/teamsfragment/TeamsFragment.kt
+++ app/src/main/java/io/github/fatimazza/footballclub/teamsfragment/TeamsFragment.kt
@@ -9,17 +9,19 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.ProgressBar
-import android.widget.Spinner
+import android.widget.*
+import com.google.gson.Gson
 import io.github.fatimazza.footballclub.R
 import io.github.fatimazza.footballclub.main.MainAdapter
+import io.github.fatimazza.footballclub.main.MainPresenter
 import io.github.fatimazza.footballclub.model.Team
+import io.github.fatimazza.footballclub.networking.ApiRepository
 import io.github.fatimazza.footballclub.utils.invisible
 import io.github.fatimazza.footballclub.utils.visible
 import org.jetbrains.anko.*
 import org.jetbrains.anko.recyclerview.v7.recyclerView
 import org.jetbrains.anko.support.v4.ctx
+import org.jetbrains.anko.support.v4.onRefresh
 import org.jetbrains.anko.support.v4.swipeRefreshLayout
 
 class TeamsFragment: Fragment(), TeamsView {
@@ -30,11 +32,19 @@ class TeamsFragment: Fragment(), TeamsView {
     private lateinit var spinner: Spinner
 
     private var teams: MutableList<Team> = mutableListOf()
-
+    private lateinit var presenter: MainPresenter
     private lateinit var adapter: MainAdapter
+
+    private lateinit var leagueName: String
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        initSpinner()
+        initAdapter()
+        initPresenter()
+        getDataOnSpinnerClicked()
+        refreshSwipeRefresh()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -68,6 +78,42 @@ class TeamsFragment: Fragment(), TeamsView {
                     }
                 }
             }
+        }
+    }
+
+    private fun initSpinner() {
+        val spinnerItems = resources.getStringArray(R.array.league)
+        val spinnerAdapter = ArrayAdapter(ctx, android.R.layout.simple_spinner_dropdown_item, spinnerItems)
+        spinner.adapter = spinnerAdapter
+    }
+
+    private fun initAdapter() {
+        adapter = MainAdapter(teams)
+        listTeam.adapter = adapter
+    }
+
+    private fun initPresenter() {
+        val request = ApiRepository()
+        val gson = Gson()
+        presenter = MainPresenter(this, request, gson)
+    }
+
+    private fun getDataOnSpinnerClicked() {
+        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+
+            override fun onItemSelected(
+                    parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                leagueName = spinner.selectedItem.toString()
+                presenter.getTeamList(leagueName)
+            }
+            override fun onNothingSelected(parent: AdapterView<*>) { }
+
+        }
+    }
+
+    private fun refreshSwipeRefresh() {
+        swipeRefresh.onRefresh {
+            presenter.getTeamList(leagueName)
         }
     }
 
