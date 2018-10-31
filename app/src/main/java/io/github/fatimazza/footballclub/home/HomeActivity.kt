d5fe80154f1f286acc8e8fb0773536390d37266b
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-10-31 18:22:15 +0700

    Home Activity - Add new Activity with XML layout

---
+++ app/src/main/java/io/github/fatimazza/footballclub/home/HomeActivity.kt
@@ -0,0 +1,13 @@
+package io.github.fatimazza.footballclub.home
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import io.github.fatimazza.footballclub.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
