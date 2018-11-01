2d6bd0b644dae261fca00e1bc4aa16ba4936a976
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-11-02 06:06:06 +0700

    Favorites Fragment - Init Adapter for List, Add Intent to open Team Detail when List clicked

--- app/src/main/java/io/github/fatimazza/footballclub/home/FavoritesFragment.kt
+++ app/src/main/java/io/github/fatimazza/footballclub/home/FavoritesFragment.kt
@@ -9,7 +9,10 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import io.github.fatimazza.footballclub.R
 import io.github.fatimazza.footballclub.R.color.colorAccent
+import io.github.fatimazza.footballclub.database.Favorite
+import io.github.fatimazza.footballclub.teamdetail.TeamDetailActivity
 import org.jetbrains.anko.*
 import org.jetbrains.anko.recyclerview.v7.recyclerView
 import org.jetbrains.anko.support.v4.ctx
@@ -20,8 +23,17 @@ class FavoritesFragment: Fragment(), AnkoComponent<Context> {
     private lateinit var swipeRefresh: SwipeRefreshLayout
     private lateinit var listFavorite: RecyclerView
 
+    private var favorites: MutableList<Favorite> = mutableListOf()
+    private lateinit var adapter: FavoritesAdapter
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        adapter = FavoritesAdapter(favorites){
+            ctx.startActivity<TeamDetailActivity>(
+                    getString(R.string.intent_id) to "${it.teamId}")
+        }
+        listFavorite.adapter = adapter
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
