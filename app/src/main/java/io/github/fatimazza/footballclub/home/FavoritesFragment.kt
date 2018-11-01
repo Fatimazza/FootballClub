bf46b6fe837f784301fac36d14a6a4f4fa2a26e7
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-11-02 06:12:59 +0700

    Favorites Fragment - Display favorite data on UI, Handle SwipeRefresh

--- app/src/main/java/io/github/fatimazza/footballclub/home/FavoritesFragment.kt
+++ app/src/main/java/io/github/fatimazza/footballclub/home/FavoritesFragment.kt
@@ -12,10 +12,14 @@ import android.view.ViewGroup
 import io.github.fatimazza.footballclub.R
 import io.github.fatimazza.footballclub.R.color.colorAccent
 import io.github.fatimazza.footballclub.database.Favorite
+import io.github.fatimazza.footballclub.database.database
 import io.github.fatimazza.footballclub.teamdetail.TeamDetailActivity
 import org.jetbrains.anko.*
+import org.jetbrains.anko.db.classParser
+import org.jetbrains.anko.db.select
 import org.jetbrains.anko.recyclerview.v7.recyclerView
 import org.jetbrains.anko.support.v4.ctx
+import org.jetbrains.anko.support.v4.onRefresh
 import org.jetbrains.anko.support.v4.swipeRefreshLayout
 
 class FavoritesFragment: Fragment(), AnkoComponent<Context> {
@@ -34,6 +38,24 @@ class FavoritesFragment: Fragment(), AnkoComponent<Context> {
                     getString(R.string.intent_id) to "${it.teamId}")
         }
         listFavorite.adapter = adapter
+
+        showFavorite()
+        swipeRefresh.onRefresh {
+            favorites.clear()
+            showFavorite()
+        }
+    }
+
+    private fun showFavorite() {
+        context?.database?.use {
+            swipeRefresh.isRefreshing = false
+
+            val result = select(Favorite.TABLE_FAVORITE)
+            val favorite = result.parseList(classParser<Favorite>())
+            favorites.addAll(favorite)
+
+            adapter.notifyDataSetChanged()
+        }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
