f3312e429c2a1ebb744fff8fc14930d547e800c1
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-11-24 22:07:24 +0700

    Kotlin Coroutines - Remove Experimental Annotation

--- app/src/test/java/io/github/fatimazza/footballclub/utils/TestContextProvider.kt
+++ app/src/test/java/io/github/fatimazza/footballclub/utils/TestContextProvider.kt
@@ -1,10 +1,8 @@
 package io.github.fatimazza.footballclub.utils
 
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlin.coroutines.CoroutineContext
 
 class TestContextProvider : CoroutineContextProvider() {
-    @ExperimentalCoroutinesApi
     override val main: CoroutineContext = Dispatchers.Unconfined
 }
