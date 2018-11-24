79fa0d7c7a422772d1deff9a5ea72c6bcd56f131
Fatima Azzahro <azzahro.fatima@gmail.com>
2018-11-24 19:42:02 +0700

    Instrumentation Test - Add Test for Home

---
+++ app/src/androidTest/java/io/github/fatimazza/footballclub/home/HomeActivityTest.kt
@@ -0,0 +1,14 @@
+package io.github.fatimazza.footballclub.home
+
+import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Rule
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class HomeActivityTest {
+
+    @Rule
+    @JvmField var activityRule = ActivityTestRule(HomeActivity::class.java)
+    
+}
